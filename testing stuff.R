01b24ebe7932d08cf0774b61b8297eea18047052
gitjgoud <nsp023@gmail.com>
2022-04-06 16:53:18 -0700

    Revert "Delete .DS_Store:Zone.Identifier"
    This reverts commit 0d7bdb54111d6641f7c27b728c10404ac303bec5.

---
+++ testing stuff.R
@@ -0,0 +1,11 @@
+data.txt = readLines('./data/data_sources.txt')
+
+
+
+data.txt = read.delim('./data/data_sources.txt')
+data.txt
+
+data.txt = read_lines('./data/data_sources.txt')
+data.txt
+
+?read
